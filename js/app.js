f3ea8cb87a5262f1239fad6e68ed2c727ffa34f9
dmitry <dmitry.panferov@gmail.com>
2016-04-15 08:54:35 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-var myApp =  angular.module('myApp', ['ngMessages', 'ngResource']);
+var myApp =  angular.module('myApp', ['ngMessages', 'ngResource', 'ngRoute']);
+
+myApp.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: 'pages/main.html',
+        controller : 'mainController'
+        })
+        .when('/second/:num', {
+            templateUrl: 'pages/second.html',
+            controller : 'secondController'
+        })
+    ;
+});
 
 myApp.controller('mainController', ['$scope', '$log', '$timeout', '$filter', 'nameService', function($scope, $log, $timeout, $filter, nameService){
 
@@ -29,6 +41,13 @@ myApp.controller('mainController', ['$scope', '$log', '$timeout', '$filter', 'na
 
 }]);
 
+myApp.controller('secondController', ['$scope', '$log', '$timeout', '$filter', '$routeParams', function($scope, $log, $timeout, $filter, $routeParams){
+
+    $scope.name = 'Second';
+
+    $scope.num = $routeParams.num;
+    console.log($scope.num);
+}]);
 
 myApp.service('nameService', function(){
     var self = this;
